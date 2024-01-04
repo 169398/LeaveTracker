e08082f063afda85e28c90d17e616342ab25263e
169398 <kulubiidris@gmail.com>
2024-01-04 12:59:58 +0300

    files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,13 @@ export default async function Home() {
   </div>
 
   {isAuth && (
-    <LeaveForm />
+    <div className="flex justify-end p-4">
+      <Link href="/Sign-out">
+        <Button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300">
+          SignOut
+        </Button>
+      </Link>
+    </div>
   )}
 
   <div className="fixed top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
