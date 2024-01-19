9f2c934f441c8f0ea346c1d73024d6d41fed4d88
169398 <kulubiidris@gmail.com>
2024-01-19 07:55:51 +0300

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,7 @@ export default async function Home() {
         <Button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">
           SignUp
         </Button>
+        <h3>   Welcome to leave tracker</h3>
       </Link>
     </div>
   )}</div>
